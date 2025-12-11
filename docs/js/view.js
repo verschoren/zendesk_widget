a28d390490b9e6f1ccb79f890630677b53c4581e
Thomas Verschoren <thomas@verschoren.com>
2025-12-11 20:24:26 +0100

    colors

--- docs/js/view.js
+++ docs/js/view.js
@@ -238,7 +238,7 @@ $('document').ready(function() {
   });
 
   $('footer').replaceWith(`
-    <footer class="md:w-[calc(100%_-_288px)] w-full fixed bottom-0 bg-white" aria-labelledby="footer-heading">
+    <footer class="md:w-[calc(100%_-_288px)] w-full fixed bottom-0 bg-white dark:bg-gray-900" aria-labelledby="footer-heading">
     <div class="border-t border-gray-900/10 p-4 flex flex-col md:flex-row justify-between w-full items-start md:items-end gap-4 md:gap-0 md:pr-24">
 
         <div class="min-w-24 w-[calc(100%_-_32px)] md:w-96">
@@ -252,7 +252,7 @@ $('document').ready(function() {
         </div>
 
         <div class="flex flex-col md:flex-row gap-2 md:gap-4">
-            <p class="text-xs leading-5 text-licorice">
+            <p class="text-xs leading-5 text-licorice dark:text-white">
                 &copy; 2022–2025
                 <a class="text-blue-400 hover:text-underline hover:text-blue-700"
                    href="https://internalnote.com?utm_source=storagecalculator">
