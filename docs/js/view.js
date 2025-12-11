f937478a67e2aab3a4bbc58dc56d212802f3638a
Thomas Verschoren <thomas@verschoren.com>
2025-12-11 20:16:38 +0100

    embded

--- docs/js/view.js
+++ docs/js/view.js
@@ -118,7 +118,7 @@ $('document').ready(function() {
         { icon: '🔐', id: 'authentication', name: 'Authentication', url: 'messaging.html', title: 'Zendesk Messaging Authentication', description: 'Demo page to showcase the JWT Authentication for Zendesk Messaging'},
         { icon: '🔐', id: 'guide_messaging', name: 'Guide Authentication', url: 'guide_messaging.html', title: 'Zendesk Messaging Authentication', description: 'Demo page to showcase the JWT Authentication for Zendesk Messaging'},
         { icon: '🎨', id: 'customlauncher', name: 'Custom Launcher', url: 'customlauncher.html' , title:'Custom Launcher for Messaging', description: 'Demo page to showcase the Custom Launcher for Zendesk Messaging'},
-        { icon: '↔️', id: 'embeddable_mode', name: 'Embeddable Mode', url: 'embed.index.html' , title:'Embeddable Mode', description: 'Demo pages for the new embeddable mode'}, 
+        { icon: '↔️', id: 'embeddable_mode', name: 'Embeddable Mode', url: 'embed/index.html' , title:'Embeddable Mode', description: 'Demo pages for the new embeddable mode'}, 
         { icon: '🎨', id: 'customtheme', name: 'Custom Theme', url: 'messaging_custom.html' , title:'Customize Widget theme', description: 'Tweak colors and theming for the web widget'}, 
         { icon: '📞', id: 'voice', name: 'Voice', url: 'voice.html', title: 'Voice API for Messaging', description: 'Demo page to showcase the Voice API for Zendesk Messaging'},
         { icon: '🍿', id: 'metadata', name: ' Metadata', url: 'prefill.html', title: 'Metadata for Zendesk Messaging', description: 'Demo page to showcase the Metadata for Zendesk Messaging'},
@@ -236,4 +236,32 @@ $('document').ready(function() {
       }
     });
   });
+
+  $('footer').html(`
+    <footer class="md:w-[calc(100%_-_288px)] w-full fixed bottom-0 bg-white" aria-labelledby="footer-heading">
+    <div class="border-t border-gray-900/10 p-4 flex flex-col md:flex-row justify-between w-full items-start md:items-end gap-4 md:gap-0 md:pr-24">
+
+        <div class="min-w-24 w-[calc(100%_-_32px)] md:w-96">
+            <h1 class="text-sm font-medium pb-2">Subscribe to Internal Note</h1>
+            <script src="https://cdn.jsdelivr.net/ghost/signup-form@~0.1/umd/signup-form.min.js"
+                data-button-color="#D1F470"
+                data-button-text-color="#11110D"
+                data-site="https://internalnote.com/"
+                async>
+            </script>
+        </div>
+
+        <div class="flex flex-col md:flex-row gap-2 md:gap-4">
+            <p class="text-xs leading-5 text-licorice">
+                &copy; 2022–2025
+                <a class="text-blue-400 hover:text-underline hover:text-blue-700"
+                   href="https://internalnote.com?utm_source=storagecalculator">
+                   Internal Note
+                </a>.
+            </p>
+        </div>
+
+    </div>
+</footer>
+`)
 });
