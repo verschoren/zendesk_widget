74975a5f542c835117d4ad7c7ac55782afde1428
Thomas Verschoren (Zendesk) <thomas.verschoren@zendesk.com>
2025-12-12 10:04:01 +0100

    footer

--- docs/js/view.js
+++ docs/js/view.js
@@ -245,31 +245,31 @@ $('document').ready(function() {
     });
   });
 
-  $('footer').replaceWith(`
-    <footer class="md:w-[calc(100%_-_288px)] w-full fixed bottom-0 bg-white dark:bg-gray-900" aria-labelledby="footer-heading">
-    <div class="border-t border-gray-900/10 p-4 flex flex-col md:flex-row justify-between w-full items-start md:items-end gap-4 md:gap-0 md:pr-24">
+//   $('footer').replaceWith(`
+//     <footer class="md:w-[calc(100%_-_288px)] w-full fixed bottom-0 bg-white dark:bg-gray-900" aria-labelledby="footer-heading">
+//     <div class="border-t border-gray-900/10 p-4 flex flex-col md:flex-row justify-between w-full items-start md:items-end gap-4 md:gap-0 md:pr-24">
 
-        <div class="min-w-24 w-[calc(100%_-_32px)] md:w-96">
-            <h1 class="text-sm font-medium pb-2">Subscribe to Internal Note</h1>
-            <script src="https://cdn.jsdelivr.net/ghost/signup-form@~0.1/umd/signup-form.min.js"
-                data-button-color="#D1F470"
-                data-button-text-color="#11110D"
-                data-site="https://internalnote.com/"
-                async>
-            </script>
-        </div>
+//         <div class="min-w-24 w-[calc(100%_-_32px)] md:w-96">
+//             <h1 class="text-sm font-medium pb-2">Subscribe to Internal Note</h1>
+//             <script src="https://cdn.jsdelivr.net/ghost/signup-form@~0.1/umd/signup-form.min.js"
+//                 data-button-color="#D1F470"
+//                 data-button-text-color="#11110D"
+//                 data-site="https://internalnote.com/"
+//                 async>
+//             </script>
+//         </div>
 
-        <div class="flex flex-col md:flex-row gap-2 md:gap-4">
-            <p class="text-xs leading-5 text-licorice dark:text-white">
-                &copy; 2022–2025
-                <a class="text-blue-400 hover:text-underline hover:text-blue-700"
-                   href="https://internalnote.com?utm_source=storagecalculator">
-                   Internal Note
-                </a>
-            </p>
-        </div>
+//         <div class="flex flex-col md:flex-row gap-2 md:gap-4">
+//             <p class="text-xs leading-5 text-licorice dark:text-white">
+//                 &copy; 2022–2025
+//                 <a class="text-blue-400 hover:text-underline hover:text-blue-700"
+//                    href="https://internalnote.com?utm_source=storagecalculator">
+//                    Internal Note
+//                 </a>
+//             </p>
+//         </div>
 
-    </div>
-</footer>
-`)
+//     </div>
+// </footer>
+// `)
 });
